ef710f7010ea78316165a795d7e008c2c1aeceaa
Deep Lajpal <>
2023-03-06 16:23:20 +0530

    working with review app component

--- Using React/reviews-app/src/Components/Reviews.js
+++ Using React/reviews-app/src/Components/Reviews.js
@@ -1,18 +1,34 @@
 import { useState } from "react";
 import People from './data';
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
 
 const Reviews = ()=>{
-    const[index, setIndex]= useState(0);
+    const[index, setIndex]= useState(2);
     console.log(People);
     const{id, name, job, image, text}= People[index]
     console.log(name)
     return (
         <>
         <div className="reviews">
-            <div className="imgContainer">
-                <img src={image} alt={name} className="img profile"></img>
-                <div className="img profile-background"></div>
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img"></img>
+                <span className="quote-icon">
+                    <FaQuoteRight />
+                </span>
             </div>
+            <h4 className="person-name">{name}</h4>
+            <p className="person-job">{job}</p>
+            <p className="person-text">{text}</p>
+            <div className='btn-container'>
+                <button className='btn arrow left-arrow'>
+                <FaChevronLeft/>
+                </button>
+                <button className='btn arrow right-arrow'>
+                <FaChevronRight/>
+                </button>
+            </div>
+            <button className='btn random-btn'>surprise me</button>
         </div>
         </>
     )
