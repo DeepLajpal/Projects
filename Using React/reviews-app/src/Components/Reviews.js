09ad099f35d2e77a035d841f3df06f53692d404f
Deep Lajpal <>
2023-03-07 14:42:36 +0530

    working on accordian-app

--- Using React/reviews-app/src/Components/Reviews.js
+++ Using React/reviews-app/src/Components/Reviews.js
@@ -3,15 +3,52 @@ import People from './data';
 import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 
+
 const Reviews = ()=>{
-    const[index, setIndex]= useState(2);
-    console.log(People);
-    const{id, name, job, image, text}= People[index]
-    console.log(name)
+    const[index, setIndex]= useState(0);
+    const{name, job, image, text}= People[index]
+
+    const checkNumber=(Index)=>{
+        if(Index<0){
+            return People.length-1;
+        }
+        if(Index>People.length-1){
+            return 0;
+        }
+        return Index;
+    }
+
+    const nextReview = (e)=>{
+            setIndex(index => {
+                var newIndex=index-1;
+                return(checkNumber(newIndex))
+            });
+        
+    };
+    const previousReview = (e)=>{
+        setIndex(index => {
+            var newIndex=index+1;
+            return(checkNumber(newIndex))
+        });
+    };
+    
+    const randomReview = ()=>{
+            setIndex(index=>{
+
+            var newIndex=Math.floor(Math.random() *4);
+            if (newIndex===index){
+                newIndex=index+1;
+            }
+            return checkNumber(newIndex);
+        })
+    }
+
+    
+
     return (
         <>
         <div className="reviews">
-            <div className="img-container">
+            <div className="img-container" >
                 <img src={image} alt={name} className="person-img"></img>
                 <span className="quote-icon">
                     <FaQuoteRight />
@@ -21,14 +58,14 @@ const Reviews = ()=>{
             <p className="person-job">{job}</p>
             <p className="person-text">{text}</p>
             <div className='btn-container'>
-                <button className='btn arrow left-arrow'>
+                <button className='btn arrow left-arrow' onClick={nextReview}>
                 <FaChevronLeft/>
                 </button>
-                <button className='btn arrow right-arrow'>
+                <button className='btn arrow right-arrow' onClick={previousReview}>
                 <FaChevronRight/>
                 </button>
             </div>
-            <button className='btn random-btn'>surprise me</button>
+            <button className='btn random-btn' onClick={randomReview}>surprise me</button>
         </div>
         </>
     )
